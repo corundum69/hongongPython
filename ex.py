bd5c7db38930641733d9c4f9b18357a564546ec5
corundum69 <51014464+corundum69@users.noreply.github.com>
2020-05-25 19:03:16 +0900

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,13 +1,19 @@
-def fibonacci(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 1
-    else:
-        return fibonacci(n -1) + fibonacci(n -2)
+## map() 함수와 filter() 함수 (call_with_func.py)
+def power(item):
+    return item * item
 
-print("fibonacci(1):", fibonacci(1))
-print("fibonacci(2):", fibonacci(2))
-print("fibonacci(3):", fibonacci(3))
-print("fibonacci(4):", fibonacci(4))
-print("fibonacci(50):", fibonacci(50))
+def under_3(item):
+    return item < 3
+
+list_input_a = [1, 2, 3, 4, 5]
+
+output_a = map(power, list_input_a)
+print("# map() 함수의 실행 결과")
+print("map(power, list_input_a):", output_a)
+print("map(power, list_input_a):", list(output_a))
+print()
+
+output_b = filter(under_3, list_input_a)
+print("# filter() 함수의 실행결과")
+print("map(power, list_input_a):", output_b)
+print("map(power, list_input_a):", list(output_b))
